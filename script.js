8fc241dfa480f7125b66fd3c96064e294d835f61
Jitske <80577121+happygeekme@users.noreply.github.com>
2021-04-20 15:50:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const makePizza = function (){
+    console.log("Combine water, meal and oil and make pizza dough");
+    console.log("roll your dough out and create a pizza bottom");
+    console.log("spread some tomato sauce on the bottom");
+    console.log("add some mozarella, champignons and salami");
+    console.log("heat the oven at 200C");
+    console.log("heat your pizza for 15 minutes");
+}
+
+const makeSushi = function (){
+    console.log("cook the sushi rice");
+    console.log("put the rice in a line on your nori paper")
+    console.log("add some small pieces fish, avocado and or cucumber")
+    console.log("now roll up the nori really thight");
+    console.log("with a sharp knive cut some slices");
+    console.log("arrange the slices on a platter with some soya sauce and wasabi and enjoy your sushi")
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
